ce8ba411ef420418d15cf79dc122cced4e15050e
kjkkuk <kjk.kuk@mail.ru>
2021-08-25 12:40:30 +0300

    fix seed for team_members

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,34 +2,34 @@
 
 require 'faker'
 
-Role.find_or_create_by(role: :admin)
-Role.find_or_create_by(role: :user)
+# Role.find_or_create_by(role: :admin)
+# Role.find_or_create_by(role: :user)
+#
+# 10.times do
+#   Team.find_or_create_by(team_name: Faker::Team.name)
+# end
+#
+# User.create!(first_name: 'Dima',
+#              last_name: 'Yakutovich',
+#              email: 'kjk.kuk@mail.ru',
+#              password: '1234567',
+#              password_confirmation: '1234567')
+#
+# 10.times do
+#   first_name = Faker::FunnyName.name.split.first
+#   last_name = Faker::FunnyName.name.split.last
+#   email = Faker::Internet.email
+#   password = '1234567'
+#   User.create!(first_name: first_name,
+#                last_name: last_name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password)
+# end
 
-10.times do
-  Team.find_or_create_by(team_name: Faker::Team.name)
-end
-
-User.create!(first_name: 'Dima',
-             last_name: 'Yakutovich',
-             email: 'kjk.kuk@mail.ru',
-             password: '1234567',
-             password_confirmation: '1234567')
-
-10.times do
-  first_name = Faker::FunnyName.name.split.first
-  last_name = Faker::FunnyName.name.split.last
-  email = Faker::Internet.email
-  password = '1234567'
-  User.create!(first_name: first_name,
-               last_name: last_name,
-               email: email,
-               password: password,
-               password_confirmation: password)
-end
-
-3.times do
-  not_exist_team = Team.all.ids - TeamMember.distinct.pluck(:teams_id)
-  TeamMember.create!([{ teams_id: not_exist_team.sample,
+not_exist_team = Team.all.ids - TeamMember.distinct.pluck(:teams_id)
+not_exist_team.each do |id|
+  TeamMember.create!([{ teams_id: id,
                         users_id: User.all.ids.sample,
                         team_role: 0,
                         is_creator: true }])
