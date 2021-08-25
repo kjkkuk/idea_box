39fa52f6404eb4d430fe2335973e6568cba8a361
kjkkuk <kjk.kuk@mail.ru>
2021-08-25 12:23:36 +0300

    create seed for team_members

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,55 +2,47 @@
 
 require 'faker'
 
-# Role.find_or_create_by(role: :admin)
-# Role.find_or_create_by(role: :user)
-#
-# 15.times do
-#   Team.find_or_create_by(team_name: Faker::Team.name)
-# end
-#
-# User.create!(first_name: 'Dima',
-#              last_name: 'Yakutovich',
-#              email: 'kjk.kuk@mail.ru',
-#              password: '1234567',
-#              password_confirmation: '1234567')
-#
-# 10.times do
-#   first_name = Faker::FunnyName.name.split.first
-#   last_name = Faker::FunnyName.name.split.last
-#   email = Faker::Internet.email
-#   password = '1234567'
-#   User.create!(first_name: first_name,
-#                last_name: last_name,
-#                email: email,
-#                password: password,
-#                password_confirmation: password)
-# end
+Role.find_or_create_by(role: :admin)
+Role.find_or_create_by(role: :user)
 
-# 10.times do
-#   p team = TeamMember.where(is_creator: true)[teams_id:]
-# # p TeamMember.where().create!(teams_id: team,
-# #                                              users_id: !User.exists?,
-# #                                              team_role: )
-# end
-TeamMember.create!([
-                     { teams_id: 16, users_id: 2, team_role: 1, is_creator: false },
-                     { teams_id: 16, users_id: 3, team_role: 3, is_creator: false },
-                     { teams_id: 17, users_id: 4, team_role: 1, is_creator: false },
-                     { teams_id: 17, users_id: 5, team_role: 2, is_creator: false },
-                     { teams_id: 17, users_id: 6, team_role: 3, is_creator: false },
-                     { teams_id: 18, users_id: 7, team_role: 1, is_creator: false },
-                     { teams_id: 18, users_id: 8, team_role: 2, is_creator: false },
-                     { teams_id: 18, users_id: 9, team_role: 3, is_creator: false },
-                     { teams_id: 22, users_id: 10, team_role: 1, is_creator: false },
-                     { teams_id: 22, users_id: 11, team_role: 2, is_creator: false },
-                     { teams_id: 22, users_id: 2, team_role: 3, is_creator: false }
-                   ])
+10.times do
+  Team.find_or_create_by(team_name: Faker::Team.name)
+end
 
-# TeamMember.create!(teams_id)
+User.create!(first_name: 'Dima',
+             last_name: 'Yakutovich',
+             email: 'kjk.kuk@mail.ru',
+             password: '1234567',
+             password_confirmation: '1234567')
 
-# p User.all.each(&:id)
-# p Team::ROLES.index.sample
-# TeamMember.create!(teams_id: Team.all.each(&:id),
-#                    users_id: User.all.each(&:id), team_role: index(Team::ROLES).sample,
-#                    is_creator: false)
+10.times do
+  first_name = Faker::FunnyName.name.split.first
+  last_name = Faker::FunnyName.name.split.last
+  email = Faker::Internet.email
+  password = '1234567'
+  User.create!(first_name: first_name,
+               last_name: last_name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+3.times do
+  not_exist_team = Team.all.ids - TeamMember.distinct.pluck(:teams_id)
+  TeamMember.create!([{ teams_id: not_exist_team.sample,
+                        users_id: User.all.ids.sample,
+                        team_role: 0,
+                        is_creator: true }])
+end
+
+Team.all.each do |team|
+  roles_of_team = team.team_members.pluck(:team_role).map(&:to_sym)
+  roles_exist = Team::ROLES - roles_of_team
+  next unless roles_exist != Team::ROLES
+  roles_exist.each do |role|
+    TeamMember.create!(team_role: role,
+                       teams_id: team.id,
+                       users_id: User.all.ids.sample,
+                       is_creator: false)
+  end
+end
