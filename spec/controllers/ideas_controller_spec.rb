6287ed130e674483d7f410dbbd8d504ab6bcaa35
kjkkuk <kjk.kuk@mail.ru>
2021-09-13 12:01:54 +0300

    resolved some bugs

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Ideas', type: :controller do
   describe 'index' do
-    it 'assigns all teams as @@teams' do
+    it 'assigns all ideas as @@ideas' do
       get :index
       expect(assigns(:ideas)).to eq([idea])
     end
