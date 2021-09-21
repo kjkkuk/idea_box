818f715da6a781df9dad25ad8fbec721d98794d3
kjkkuk <kjk.kuk@mail.ru>
2021-09-21 16:34:48 +0300

    add impersonation

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -12,4 +12,14 @@ class UserPolicy < ApplicationPolicy
   def update?
     user&.id == record.id
   end
+
+  def impersonate?
+    all_members = []
+    user.team_members.where(is_creator: true).each do |tm|
+      tm.team.team_members.where(is_creator: false).each do |not_creator|
+        all_members << not_creator
+      end
+    end
+    all_members.pluck(:users_id).include?(record.id)
+  end
 end
